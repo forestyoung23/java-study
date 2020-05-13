3519d9cbe6ac4fcb5dd977823edc49b91e51ba0a
Forest <forestyoung23@gmail.com>
2020-05-14 01:18:40 +0800

    完善稀疏数组代码

--- src/com/forest/collection/array/SparseArray.java
+++ src/com/forest/collection/array/SparseArray.java
@@ -3,37 +3,88 @@ package com.forest.collection.array;
 /**
  * 稀疏数组
  *
- * @author dongyang
+ * @author Forest
  * @date 2020年05月11日 12:17
  */
 public class SparseArray {
 
     public static void main(String[] args) {
-        int[][] array = new int[11][11];
-        array[1][3] = 2;
+        int[][] array = new int[8][8];
+        array[1][3] = 1;
         array[2][4] = 2;
         array[4][7] = 3;
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
-                System.err.print(array[i][j] + " ");
+        int[][] ints = array2SparseArray(array);
+        sparseArray2Array(ints);
+    }
+
+    /**
+     * 数组转稀疏数组
+     *
+     * @param array
+     * @return
+     * @author Forest
+     * @date 2020/5/14 12:57 上午
+     */
+    private static int[][] array2SparseArray(int[][] array) {
+        // 打印原数组
+        System.err.println("原数组：");
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                System.err.print(anInt + "  ");
             }
             System.err.println();
         }
-        int validNumber = 0;
+        // 获取非零数字个数
+        int validCount = 0;
         for (int[] ints : array) {
             for (int anInt : ints) {
                 if (0 != anInt) {
-                    validNumber++;
+                    validCount++;
                 }
             }
         }
-        int[][] sparseArray = new int[validNumber + 1][3];
+        int[][] sparseArray = new int[validCount + 1][3];
         sparseArray[0][0] = array.length;
         sparseArray[0][1] = array[0].length;
-        sparseArray[0][2] = validNumber;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.err.print(sparseArray[i][j] + " ");
+        sparseArray[0][2] = validCount;
+        int count = 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (0 != array[i][j]) {
+                    sparseArray[count][0] = i;
+                    sparseArray[count][1] = j;
+                    sparseArray[count][2] = array[i][j];
+                    count++;
+                }
+            }
+        }
+        System.err.println("转换后的稀疏数组：");
+        for (int[] ints : sparseArray) {
+            for (int anInt : ints) {
+                System.err.print(anInt + " ");
+            }
+            System.err.println();
+        }
+        return sparseArray;
+    }
+
+    /**
+     * 稀疏数组转数组
+     *
+     * @param array
+     * @return
+     * @author Forest
+     * @date 2020/5/14 1:13 上午
+     */
+    private static void sparseArray2Array(int[][] array) {
+        int[][] newArray = new int[array[0][0]][array[0][1]];
+        for (int a = 1; a <= array[0][2]; a++) {
+            newArray[array[a][0]][array[a][1]] = array[a][2];
+        }
+        System.err.println("转换后的原数组");
+        for (int[] ints : newArray) {
+            for (int anInt : ints) {
+                System.err.print(anInt + "  ");
             }
             System.err.println();
         }

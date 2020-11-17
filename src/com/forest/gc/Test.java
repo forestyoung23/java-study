eea0b082e1caa36b7c26c37d36c0d620569687d9
Forest <forestyoung23@gmail.com>
2020-11-17 23:33:39 +0800

    Arrays.asList底层是数组，不能使用add

--- src/com/forest/gc/Test.java
+++ src/com/forest/gc/Test.java
@@ -1,11 +1,15 @@
 package com.forest.gc;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Forest
  * @date 2020年11月08日 15:52
  */
 public class Test {
     boolean checkedOut = false;
+
     public Test(boolean checkedOut) {
         this.checkedOut = checkedOut;
     }
@@ -20,5 +24,11 @@ public class Test {
             System.err.println("gc错误");
         }
 //             super.finalize();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        list.add(6);
+        System.err.println(list);
     }
 }

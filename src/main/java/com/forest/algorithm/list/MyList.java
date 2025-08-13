6b207e95d34a699da04b1dd355c0105643288f33
Forest <forestyoung23@gmail.com>
2025-08-13 23:26:31 +0800

    自定义线性表方法定义

---
+++ src/main/java/com/forest/algorithm/list/MyList.java
@@ -0,0 +1,19 @@
+package com.forest.algorithm.list;
+
+/**
+ * 自定义线性表
+ *
+ * @author Forest Dong
+ * @date 2025年08月13日 23:23
+ */
+public class MyList {
+    // 初始化
+    // 销毁
+    // 清空
+    // 插入（指定位置插入）
+    // 删除（指定位置删除）
+    // 判空
+    // 返回元素个数
+    // 获取指定元素的下标
+    // 获取指定位置的元素
+}
